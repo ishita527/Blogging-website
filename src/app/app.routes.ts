68bd079827267d30679a1389a9fe712c026a1c34
Ishita <ishitahariramani@gmail.com>
2025-10-02 01:11:23 +0530

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { Login} from './pages/login/login';
-import { Home} from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
 import { CreateBlog } from './pages/create-blog/create-blog';
 
 export const routes: Routes = [
   { path: '', component: Login, pathMatch: 'full' },
   { path: 'home', component: Home },
-  { path: 'create-blog/:id', component: CreateBlog },
-   { path: 'create-blog', component: CreateBlog },
+  { path: 'create-blog/:id', component: CreateBlog, data: { renderMode: 'ssr' } },
+  { path: 'create-blog', component: CreateBlog },
 ];
